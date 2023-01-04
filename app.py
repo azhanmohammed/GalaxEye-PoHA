acb1b5981a899653ab0d44d14a5026381c25a5fb
Azhan Mohammed <46521547+sheikhazhanmohammed@users.noreply.github.com>
2023-01-04 14:11:30 +0530

    updated app

--- app.py
+++ app.py
@@ -27,16 +27,36 @@ with st.sidebar:
     )
 
 logo = Image.open("./static/galaxeye.png")
-if choose == "Dashboard":
-    col1, col2 = st.columns( [0.8, 0.2])
-    with col1:               # To display the header text using css style
-        st.markdown(""" <style> .font {
-        font-size:35px ; font-family: 'Cooper Black'; color: #ffffff;} 
-        </style> """, unsafe_allow_html=True)
-        st.markdown('<p class="font">Detailed Analysis</p>', unsafe_allow_html=True)    
-    with col2:               # To display brand log
-        st.image(logo, width=130 )
+# if choose == "Dashboard":
+#     col1, col2 = st.columns( [0.8, 0.2])
+#     with col1:               # To display the header text using css style
+#         st.markdown(""" <style> .font {
+#         font-size:35px ; font-family: 'Cooper Black'; color: #ffffff;} 
+#         </style> """, unsafe_allow_html=True)
+#         st.markdown('<p class="font">Detailed Analysis</p>', unsafe_allow_html=True)    
+#     with col2:               # To display brand log
+#         st.image(logo, width=130 )
     
+#     map = folium.Map(location=[-2.4474679490380993, -79.98500168575568], zoom_start=16, scrollWheelZoom=True, tiles='CartoDB positron')
+#     img = folium.raster_layers.ImageOverlay(
+#         name="RGB Image",
+#         image="./static/rgbimage.png",
+#         bounds=[[-2.460891311086019, -79.99976724386217], [-2.4354694248845985, -79.96761258691551]],
+#         opacity=1.0,
+#         interactive=True,
+#         cross_origin=False,
+#         zindex=1,
+#     )
+
+#     img.add_to(map)
+#     for l, row in enumerate(dataframe.iterrows()):
+#             html = popupTable(dataframe, l)
+#             iframe = branca.element.IFrame(html=html,width=700,height=600)
+#             popup = folium.Popup(folium.Html(html, script=True), max_width=500)
+#             folium.GeoJson(data=row[1][5], popup=popup).add_to(map)
+#     st_map = st_folium(map, width=1000, height=650)
+
+if choose == "Dashboard":
     map = folium.Map(location=[-2.4474679490380993, -79.98500168575568], zoom_start=16, scrollWheelZoom=True, tiles='CartoDB positron')
     img = folium.raster_layers.ImageOverlay(
         name="RGB Image",
